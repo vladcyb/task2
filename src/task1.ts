e6b687ef23b96b636c811a9db64e076a281f9937
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-01-20 15:57:40 +0300

    fix(task1): Убрал лишний код

--- src/task1.ts
+++ src/task1.ts
@@ -40,17 +40,6 @@ const _ = {
       return false;
     }
 
-    if (Array.isArray(obj1)) {
-      if (obj1.length !== obj2.length) {
-        return false;
-      }
-      for (let i = 0; i < obj1.length; i++) {
-        if (!_.isEqual(obj1[i], obj2[i])) {
-          return false;
-        }
-      }
-      return true;
-    }
     for (let key in obj1) {
       if (!_.isEqual(obj1[key], obj2[key])) {
         return false;
