e4a9290368843724d955543eefad671afea5b398
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-01-26 18:33:13 +0300

    fix(task4): Доделал

--- src/task4.js
+++ src/task4.js
@@ -18,7 +18,7 @@ const API = {
   nonStableFetch: (isStable) => {
     return new Promise((resolve, reject) => {
       if (getRandomBoolean() || isStable) {
-        resolve({ data: { x: 1, yasdf: '323' } });
+        resolve({ data: { x: 1, yasdf: '323' }, method: 'nonStableFetch' });
       } else {
         reject('[nonStableFetch]: Network error!');
       }
@@ -30,7 +30,7 @@ const API = {
         reject('[fakeFetch]: x not provided');
       } else if (getRandomBoolean()) {
         setTimeout(() => {
-          resolve({ data: { value: Math.random() } });
+          resolve({ data: { value: Math.random() }, method: 'fakeFetch' });
         }, 500);
       } else {
         reject('[fakeFetch]: Network error!');
@@ -40,10 +40,31 @@ const API = {
   longFetch: () => {
     return new Promise((resolve) => {
       setTimeout(() => {
-        resolve({ ok: true });
+        resolve({ ok: true, method: 'longFetch' });
       }, 1500);
     });
   },
+  oneSecondFetch: () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({method: 'oneSecondFetch'});
+      }, 1000)
+    });
+  },
+  twoSecondsFetch: () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({method: 'twoSecondsFetch'});
+      }, 2000)
+    });
+  },
+  threeSecondsFetch: () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({method: 'threeSecondsFetch'});
+      }, 3000)
+    });
+  }
 };
 
 const promiseAll = (promises) => {
@@ -62,7 +83,19 @@ const promiseAll = (promises) => {
   });
 };
 
-const test2 = async () => {
+const promiseRace = (promises) => {
+  return new Promise((resolve, reject) => {
+    promises.forEach(async (promise) => {
+      try {
+        resolve(await promise);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  });
+};
+
+const promiseAllTest = async () => {
   try {
     const result = await promiseAll([
       API.fakeFetch(1),
@@ -75,5 +108,19 @@ const test2 = async () => {
   }
 };
 
+const promiseRaceTest = async () => {
+  try {
+    const result = await promiseRace([
+      API.threeSecondsFetch(),
+      API.twoSecondsFetch(),
+      API.oneSecondFetch(),
+    ]);
+    console.log(JSON.stringify(result, null, 2));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-test2();
+promiseAllTest();
+
+// promiseRaceTest();
